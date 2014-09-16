fd236572206481ea4f9eef221fca5b5e37fa8576
meiroo <meiroo@outlook.com>
2014-09-16 23:33:01 +0800

    debug file upload to server

--- ImageService/app.js
+++ ImageService/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var busboy = require('connect-busboy');
 
 var routes = require('./routes/index');
 var upload = require('./routes/file-upload');
@@ -19,8 +20,9 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+app.use(busboy()); 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- ImageService/routes/file-upload.js
+++ ImageService/routes/file-upload.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET users listing. */
 router.post('/', function(req, res) {
-	console.log('!!file upload!!');
-  	res.send('respond with a resource');
+	req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+        console.log("Uploading: " + filename); 
+        console.log(file);
+        fstream = fs.createWriteStream(__dirname + '/files/' + filename);
+        file.pipe(fstream);
+        fstream.on('close', function () {
+            res.redirect('back');
+        });
+        //res.send('respond with a resource');
+    });
 });
 
 module.exports = router;
